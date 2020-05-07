439e42fd657c3b0a433c885ec5b9ab797222d986
dcl_glb <dcl@qq.com>
2020-05-07 23:29:50 +0800

    希尔排序/缩小增量排序[插入排序的升级]

---
+++ Sort/src/com/dcl/ShellSort.java
@@ -0,0 +1,43 @@
+package com.dcl;
+
+import java.util.Arrays;
+
+/**
+ * @author dcl
+ * @date 2020/5/7 - 22:56
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        //int arr[] = {7, 4, 40, 5, 8, 3, 15, 28, 39, 23,11};
+
+        int arr[] = new int[8000000];
+        for (int i = 0; i < 8000000; i++) {
+            arr[i] = (int)(Math.random()*1000000000);
+        }
+        long start = System.currentTimeMillis();
+        shellSort(arr);
+        long end = System.currentTimeMillis();
+        shellSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("使用多少时间："+(end-start)+"ms");
+    }
+
+    public static void shellSort(int[] arr) {
+
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int index = i - gap;
+                int tmp = arr[i];
+                while (index >= 0 && tmp < arr[index]) {
+                    arr[index + gap] = arr[index];
+                    index -= gap;
+                }
+                if (index != i - gap) {
+                    arr[index + gap] = tmp;
+                }
+            }
+        }
+    }
+
+}
